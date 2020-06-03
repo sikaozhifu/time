46b88c35526e39fa19267f4c7e4a0a021439f777
sikaozhifu <1179979738@qq.com>
2020-06-03 13:10:47 +0800

    添加配置文件的代码

---
+++ src/main/java/com/real/time/config/MyConfig.java
@@ -0,0 +1,26 @@
+package com.real.time.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class MyConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+    }
+
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+    }
+}
